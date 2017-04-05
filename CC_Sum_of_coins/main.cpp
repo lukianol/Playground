10627fbea5f03f863ea030e4c1f17faa03f964eb
lukianol <alexander.lukyantsev@gmail.com>
2017-04-04 23:08:52 -0700

    Added code

--- CC_Sum_of_coins/main.cpp
+++ CC_Sum_of_coins/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <set>
 
 using namespace std;
 
+/**
+ * @brief getSums
+ * @param coin1
+ * @param coin2
+ * @param coin3
+ * @return
+ * @time O(1)
+ * @memory O(1)
+ */
+set<int> getSums(uint coin1, uint coin2, uint coin3) {
+    set<int> results;
+
+    results.insert(0);
+
+    for(int i = 1; i <= 1000; i++) {
+        if (results.find(i - coin1) != results.end()
+                || results.find(i - coin2) != results.end()
+                || results.find(i - coin3) != results.end()) {
+            results.insert(i);
+        }
+    }
+
+    return results;
+}
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    set<int> sums = getSums(10, 15, 55);
+    set<int>::iterator iterator = sums.begin();
+
+    while(iterator != sums.end()){
+        cout << *iterator << endl;
+        iterator++;
+    }
     return 0;
 }
+
+
---
+++ CC_Sum_of_coins/readme.md
@@ -0,0 +1,19 @@
+Task:
+
+/**
+Given many coins of 3 different face values, print the combination sums of the coins up to 1000. Must be printed in order.
+
+eg: coins(10, 15, 55)
+print:
+10
+15
+20
+25
+30
+.
+.
+.
+1000
+*/
+
+Link: https://www.careercup.com/question?id=5729312817807360
