86b1d604d53c144e9d00b6989fb38cf23815121d
lukianol <alexander.lukyantsev@gmail.com>
2017-04-22 01:24:55 -0700

    Finished generic implementation of Kadane algorithm

--- Common/Algortihms/KadaneAlgorithm.java
+++ Common/Algortihms/KadaneAlgorithm.java
@@ -1,24 +1,47 @@
 public class KadaneAlgorithm {
-	public static int[] findSubArrayWithMaxSum(int[] array) {
+		
+	public static int[] findSubArrayWithMaxSum(int[] array) throws Exception {
+		return findSubArrayWithMaxSum(array, 0);
+	}
+	
+	public static int[] findSubArrayWithMaxSum(int[] array, int subArrayMinSize) throws Exception {
 		if (array == null) {
 			return null;
 		}
 		
-		if (array.length == 0) {
+		if (subArrayMinSize < 0) {
+			throw new IllegalArgumentException("subArrayMinSize cannot be less than zero.");
+		}
+		
+		if (array.length < subArrayMinSize) {
+			throw new Exception("Cannot return subArray of specified min size.");
+		}
+		
+		if (subArrayMinSize == array.length) {
 			return array.clone();
 		}
 		
+		if (subArrayMinSize == 0) {
+			subArrayMinSize = 1;
+		}
+		
 		int start = 0;
-		int end = 0;
+		int end = subArrayMinSize - 1;
 		int newStart = 0;
-		int currentMax = array[0];
-		int maxSoFar = array[0];
-		for(int i = 0; i < array.length; i++) {
+		int currentMax = 0;
+		for(int i = 0; i < subArrayMinSize; i++) {
+			currentMax += array[i];
+		}
+		int maxSoFar = currentMax;
+		int lastValues = currentMax - array[0];
+		
+		for(int i = subArrayMinSize; i < array.length; i++) {
 			int value = array[i];
 			int newMax = value + currentMax;
-			if (value > newMax) {
-				currentMax = value;
-				newStart = i;
+			int subArrayStart = i - (subArrayMinSize - 1);
+			if (value + lastValues > newMax) {
+				currentMax = value + lastValues;
+				newStart = subArrayStart;
 			} else {
 				currentMax = newMax;
 			}
@@ -27,6 +50,8 @@ public class KadaneAlgorithm {
 				end = i;
 				maxSoFar = currentMax;
 			}
+			lastValues += value;
+			lastValues -= array[subArrayStart];
 		}
 		
 	    int[] newArray = new int[end - start + 1];
--- Common/Algortihms/KadaneAlgortihmTests.java
+++ Common/Algortihms/KadaneAlgortihmTests.java
@@ -1,14 +1,66 @@
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 public class KadaneAlgortihmTests {
 
 	@Test
-	public void findSubArrayWithMaxSum_AllNegatives_ReturnsTheGreatest() {
+	public void findSubArrayWithMaxSum_AllNegatives_Greatest() throws Exception {
 		int [] allNegatives = {-2, -1, -3, -4, -1};
 		int[] subArray = KadaneAlgorithm.findSubArrayWithMaxSum(allNegatives);
 		assertArrayEquals(new int[]{-1}, subArray);		
 	}
-
+	
+	@Test
+	public void findSubArrayWithMaxSum_AllPositive_WholeArray() throws Exception {
+		int [] allPositives = {2, 1, 3, 4, 1};
+		int[] subArray = KadaneAlgorithm.findSubArrayWithMaxSum(allPositives);
+		assertArrayEquals(allPositives, subArray);		
+	}
+	
+	@Test
+	public void findSubArrayWithMaxSum_Any_CopyOfArray() throws Exception {
+		int [] allPositives = {2, 1, 3, 4, 1};
+		int[] subArray = KadaneAlgorithm.findSubArrayWithMaxSum(allPositives);
+		assertNotSame(allPositives, subArray);		
+	}
+	
+	@Test
+	public void findSubArrayWithMaxSum_Mix_OnlyPositiveSubarray() throws Exception {
+		int [] mix = {-5,4,10,-2,-20};
+		int[] subArray = KadaneAlgorithm.findSubArrayWithMaxSum(mix);
+		assertArrayEquals(new int[] {4, 10}, subArray);		
+	}
+	
+	@Test
+	public void findSubArrayWithMaxSumAndAtLeastTwoElements_Misc_Misc() throws Exception {
+		
+		HashMap<int[], int[]> map = new HashMap<int[], int[]>();
+		map.put(new int[]{-2, -1, -3, -4, -1}, new int[]{-2, -1});
+		map.put(new int[]{-2, -1, -3, 4, -1}, new int[]{4, -1});
+		map.put(new int[]{-2, -1, -3, 4, 1}, new int[]{4, 1});
+		map.put(new int[]{-5,4,-2,10}, new int[]{4, -2, 10});
+		
+		for(HashMap.Entry<int[], int[]> entry : map.entrySet()) {
+			int[] subArray = KadaneAlgorithm.findSubArrayWithMaxSum(entry.getKey(), 2);
+			assertArrayEquals(entry.getValue(), subArray);
+		}	
+	}
+	
+	@Test
+	public void findSubArrayWithMaxSumAndAtLeastThreeElements_Misc_Misc() throws Exception {
+		
+		HashMap<int[], int[]> map = new HashMap<int[], int[]>();
+		map.put(new int[]{-2, -1, -3, -4, -1}, new int[]{-2, -1, -3});
+		map.put(new int[]{-2, -1, -3, 4, -1}, new int[]{-1, -3, 4});
+		map.put(new int[]{-2, -1, -3, 4, 1}, new int[]{-3, 4, 1});
+		map.put(new int[]{-5,  4, -2, 10}, new int[]{4, -2, 10});
+		
+		for(HashMap.Entry<int[], int[]> entry : map.entrySet()) {
+			int[] subArray = KadaneAlgorithm.findSubArrayWithMaxSum(entry.getKey(), 3);
+			assertArrayEquals(entry.getValue(), subArray);
+		}	
+	}
 }
